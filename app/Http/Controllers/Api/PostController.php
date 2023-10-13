a0a5c2e2c411c5388944f8377ca548da7448e30b
Magd <magdelzalameh6@gmail.com>
2023-10-14 01:54:37 +0300

    new edits

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    use ApiResponseTrait;
+
     public function index(){
-        $posts = Post::get();
-        $msg = ["ok"];
-        return response($posts,200,$msg);
+        //collection for many posts
+        $posts = PostResource::collection(Post::get());
+        return $this->apiResponse($posts,'success',200);
+    }
+
+    public function show($id){
+
+        $post = new PostResource(Post::find($id));
+
+        if($post){
+            return $this->apiResponse($post,'ok',200);
+        }
+
+        return $this->apiResponse(null,'The Post Not Found',401);
+
     }
 }
