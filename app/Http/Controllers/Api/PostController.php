68e5ab39e1e5835bad5d2473e7f9eec3fe0df1a5
Magd <magdelzalameh6@gmail.com>
2023-10-15 00:31:10 +0300

    validation

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -30,6 +31,18 @@ class PostController extends Controller
     }
 
     public function store(Request $request){
+
+        $validator = Validator::make($request->all(),[
+
+            'title' => 'required|unique:posts|max:255',
+            'body' => 'required',
+
+        ]);
+
+        if($validator->fails()){
+
+            return $this->apiResponse(null,$validator->errors(),400);
+        }
 
         $post = Post::create($request->all());
 
